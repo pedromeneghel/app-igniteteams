6fcba866a1562b77af59b127aaa4ff4972a41151
Pedro Meneghel <pedromeneghel@gmail.com>
2023-07-25 14:42:45 -0300

    feat: adiciona screen NewGroup

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { Loading } from '@components/Loading';
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
-import { Groups } from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroup';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme';
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style="light" translucent />
-     {fontsLoaded ? <Groups /> : <Loading />}
+     {fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   );
 }
