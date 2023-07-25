c20e7631748d618f7e94ea31ed17f38606ed3f9f
Pedro Meneghel <pedromeneghel@gmail.com>
2023-07-25 16:27:00 -0300

    feat: adiciona componente Button Icon

---
+++ src/components/ButtonIcon/styles.ts
@@ -0,0 +1,30 @@
+import { TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+import { MaterialIcons } from "@expo/vector-icons"
+
+export type ButtonIconTypeStyleProps = 'PRIMARY' | 'SECONDARY';
+
+type Props = {
+  type: ButtonIconTypeStyleProps;
+}
+
+export const Container = styled(TouchableOpacity)`
+  width: 56px;
+  height: 56px;
+  justify-content: center;
+  align-items: center;
+  margin-left: 12px;
+`;
+
+export const Icon = styled(MaterialIcons).attrs<Props>(({ theme, type }) => ({
+  size: 24,
+  color: type === 'PRIMARY' ? theme.COLORS.GREEN_700 : theme.COLORS.RED,
+}))``;
+
+export const Form = styled.View`
+  width: 100%;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
+  flex-direction: row;
+  justify-content: center;
+  border-radius: 6px;
+`;
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -2,13 +2,35 @@ import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Input";
+import { Form } from "@components/ButtonIcon/styles";
 
 export function Players() {
   return (
     <Container>
-      <Header showBackButton></Header>
-      <Highlight title="Nome da turma" subtitle="Adicione a galera e separe os times" />
-      <Button title="Remover turma" type="SECONDARY" />
+      <Header showBackButton />
+
+      <Highlight 
+        title="Nome da turma" 
+        subtitle="Adicione a galera e separe os times" 
+      />
+
+      <Form>
+        <Input
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+
+        <ButtonIcon 
+          icon="add" 
+        />
+      </Form>
+
+      <Button 
+        title="Remover turma" 
+        type="SECONDARY" 
+      />
     </Container>
   )
 }
