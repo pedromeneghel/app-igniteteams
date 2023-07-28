8c37018ea49c9b9956f7b2772e4d781dd27aac26
Pedro Meneghel <pedromeneghel@gmail.com>
2023-07-28 15:47:09 -0300

    feat: implementa funcionalidade de remoção de pessoas

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -14,6 +14,7 @@ import { AppError } from "@utils/AppError";
 import { useEffect, useRef, useState } from "react";
 import { Alert, FlatList, TextInput } from "react-native";
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
+import { playerRemoveByGroup } from "@storage/player/playerRemoveByGroup";
 
 type RouteParams = {
   group: string;
@@ -63,6 +64,16 @@ export function Players() {
     } catch(error) {
       console.log(error);
       Alert.alert('Pessoas', 'Não foi possível carregar as pessoas do time selecionado.')
+    }
+  }
+
+  async function handlePlayerRemove(playerName: string){
+    try {
+      await playerRemoveByGroup(playerName, group);
+      fetchPlayersByTeam();
+    } catch(error) {
+      console.log(error);
+      Alert.alert('Remover pessoa', 'Não foi possível remover essa pessoa.');
     }
   }
 
@@ -119,7 +130,7 @@ export function Players() {
         renderItem={({ item }) => (
           <PlayerCard
             name={item.name}
-            onRemove={() => { }}
+            onRemove={() => handlePlayerRemove(item.name)}
           />
         )}
         ListEmptyComponent={() =>
